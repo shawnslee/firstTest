fef79c42bb1c9acde0469ed6554bd4aacd7868b3
Shawn Lee <shawn.lee@tigerspike.com>
2014-11-04 14:52:58 -0500

    Initial Commit

---
+++ testingAlerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testingAlerView
+//
+//  Created by Shawn Lee on 11/4/14.
+//  Copyright (c) 2014 Shawn Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
